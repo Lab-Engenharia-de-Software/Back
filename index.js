3e84e0c68af71ec4d0ddd2869ada864b3267f055
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-10-02 10:11:30 -0300

    test Heroku

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 //const { PrismaClient } = require('@prisma/client')
 //const prisma = new PrismaClient();
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-var administrador = require('./routes/administrador') //var para uso do endpoint
-var login = require('./routes/login') 
+var administrador = require('./routes/administrador'); //var para uso do endpoint
+var login = require('./routes/login');
+const port = process.env.PORT || 3333;
 
-app.use(express.json()) //create server
+app.use(express.json()); //create server
 
 //route endpoints
-app.use('/administrador',administrador)
-app.use('/login', login)
+app.use('/administrador',administrador);
+app.use('/login', login);
 
 app.get('/home', async (req, res) => {
-  res.json({"message":"test da home"})
-  console.log('sim')
+  res.json({"message":"test da home com heroku"})
+  console.log('sim'); 
 })
 
-app.listen(3333,() => {
-  console.log('porta 3333 on')
+app.listen(port,() => {
+  console.log('porta 3333 on');
 })
-console.log('XD')
+console.log('XD');
 
-module.exports = app
+module.exports = app;
