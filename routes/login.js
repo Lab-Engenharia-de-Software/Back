77483da92c9d7cdc5b1fe923a66255a48c2ac66b
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-10-03 13:59:40 -0300

    login de administrador, secretaria e pesquisador

--- routes/login.js
+++ routes/login.js
@@ -8,8 +8,6 @@ route.post('/', async (req, res) => {
     try{
         console.log("body endiado ",req.body)
         //caso seja adm
-        let accessPesquisador = null
-        let accessSecretaria = null 
         let accessADM = await prisma.adm.findFirst({
             where: {
                 email: {
@@ -20,47 +18,80 @@ route.post('/', async (req, res) => {
                 }
             }
         })
-
+        if(accessADM == null){
+            accessADM = {id:"1"}
+        }
         //caso seja um pesquisador (ainda nao implementado)
-        {/*if (accessADM == null){
-            let accessPesquisador = await prisma.Pesquisador.findFirst({
-                where: {
-                    email: {
-                        equals: req.body.email
-                    },
-                    senha: {
-                        equals: req.body.email
-                    }
+        let accessPesquisador = await prisma.pesquisadores.findFirst({
+            where: {
+                email: {
+                    equals: req.body.email
+                },
+                senha: {
+                    equals: req.body.senha
                 }
-            })
-        } */}
-        console.log(accessADM)
+            }
+        })
+        if(accessPesquisador == null){
+            accessPesquisador = {id:"1"}
+        }
 
-        if ( accessADM == null || accessADM.id == "1"){
+        let accessSecretaria = await prisma.secretarias.findFirst({
+            where: {
+                email: {
+                    equals: req.body.email
+                },
+                senha: {
+                    equals: req.body.senha
+                }
+            }
+        })
+        if(accessSecretaria == null){
+            accessSecretaria = {id:"1"}
+        }
+        //id 1 é pq n tem cadastro
+        if ( accessADM.id == "1" 
+        & accessPesquisador.id == "1"
+        & accessSecretaria.id == "1"){
             res.json({
                 "message": "Dados inválidos!",
                 "erro": "1"
             })
             
-        } else{
+        } else if (accessADM.id != "1"){
             res.json({
-                "matricula":`${accessADM.id}`,
+                "id":`${accessADM.id}`,
                 "nome": `${accessADM.nome}`,
                 "email": `${accessADM.email}`,
                 "cpf":`${accessADM.cpf}`,
                 "cargo":`${accessADM.role}`,
                 "status":`${accessADM.status}`
             })
+        }  else if (accessPesquisador.id != "1"){
+            res.json({
+                "id":`${accessPesquisador.id}`,
+                "nome": `${accessPesquisador.nome}`,
+                "email": `${accessPesquisador.email}`,
+                "cpf":`${accessPesquisador.cpf}`,
+                "cargo":`${accessPesquisador.role}`,
+                "status":`${accessPesquisador.status}`
+            })
+        } else{
+            res.json({
+                "id":`${accessSecretaria.id}`,
+                "nome": `${accessSecretaria.nome}`,
+                "email": `${accessSecretaria.email}`,
+                "cpf":`${accessSecretaria.cpf}`,
+                "cargo":`${accessSecretaria.role}`,
+                "status":`${accessSecretaria.status}`
+            })
         }
-
     }catch(e){ 
         console.log(e)
         res.json({
             "message": "erro"
         })
-
-    }
-    
+    }   
 })
 
 module.exports = route
