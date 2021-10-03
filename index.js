c07bb1fade9100987795bb599b94690215075e0b
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-10-03 12:14:51 -0300

    endpoint para cadastro de secretaria

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(cors())
 app.use('/administrador',administrador);
 app.use('/login', login);
 app.use('/Pesquisador', pesquisadores)
+app.use('/Secretaria', secretaria);
 
 app.get('/home', async (req, res) => {
   res.json({"message":"test da home com heroku "})
