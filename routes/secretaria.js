7eb75167670286b06e2bade073e89d4eb16d2e80
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-10-06 21:58:12 -0300

    Ajuste no cadastro de secretaria
    Co-authored-by: Matheus Rodrigues dos Santos <muctoz@users.noreply.github.com>
    Co-authored-by: Vinícius Irikawa <viniciusIrikawa@users.noreply.github.com>
    Co-authored-by: Vitor Luan <VitorLuanS@users.noreply.github.com>
    Co-authored-by: Malakaba <Malakaba@users.noreply.github.com>

--- routes/secretaria.js
+++ routes/secretaria.js
@@ -5,26 +5,56 @@ var route = express.Router()
 //cadastro de secretaria
 route.post('/Cadastro', async (req, res) => {
     console.log('cadastro secretaria')
-    
-    if (req.headers.authorization === 'admin') {
-        let secretaria = await prisma.secretarias.create({
-            data: {
-                email: req.body.email,
-                senha: req.body.senha,
-                nome: req.body.nome,
-                cpf: req.body.cpf,
-                telefone: req.body.telefone,
-                endereco: req.body.endereco,
-                role: "secretaria"
-    
+    try{
+        let validateAdm = await prisma.adm.findFirst({
+            where: {
+                email: {
+                    equals: req.body.email
+                },
             }
         })
-        res.json({"message": `Cadatrado a secretaria ${secretaria.nome}, com id ${secretaria.id}`})
-    
-    }else{
-        res.json({"message": `Erro: ${req.headers.authorization} não possui autorização para esta ação`})
+        if(validateAdm == null){
+            validateAdm = {id:"1"}
+        }
+        
+        let validatePesquisador = await prisma.secretarias.findFirst({
+            where: {
+                email: {
+                    equals: req.body.email
+                },
+            }
+        })
+        if(validatePesquisador == null){
+            validatePesquisador = {id:"1"}
+        }
+        console.log(validateAdm, validatePesquisador)
+        if(validateAdm.id == "1" & validatePesquisador.id =="1" & req.body.authorization == 'admin'){
+            let secretaria = await prisma.secretarias.create({
+                data: {
+                    email: req.body.email,
+                    senha: req.body.senha,
+                    nome: req.body.nome,
+                    cpf: req.body.cpf,
+                    telefone: req.body.telefone,
+                    endereco: req.body.endereco,
+                    role: "secretaria"
+                }
+            })
+            res.json({"message": `Cadatrado a secretaria ${secretaria.nome}, com id ${secretaria.id}`,
+                      "status": "2"})
+            
+        }else{
+            res.json({"message": `Erro: ${req.body.authorization} não possui autorização para esta ação`,
+                        "status":"1"})
+        }
+    }catch(e){
+        console.log(e)
+
+    }finally{
+        res.json({"message":"Erro de cadastro",
+                  "status": "0"})
     }
-    
+   
 })
 
 module.exports = route
