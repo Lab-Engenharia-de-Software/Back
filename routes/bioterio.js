6e49632f1d1abfe547418fb63a06adfd3fef7d55
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-10-27 18:14:27 -0300

    Endpoint para cadastro de biotério e espécie - joao pedro

--- routes/bioterio.js
+++ routes/bioterio.js
@@ -6,17 +6,47 @@ var route = express.Router()
 route.post('/Cadastro', async (req, res) => {
     console.log('cadastro de bioterio')
     try {
+        let bioterio = await prisma.bioterios.create({
+            data: {
+                nome: req.body.nome,
+            },
+        })
+        res.json({"message":"biotério cadastrado",
+                "status":"success"})
+        }
+
+    catch(e){
+        console.log(e)
+        console.log("body recebido:" + req.body)
+    } finally{
+        res.json({"message":"Ocorreu um erro em cadastro de bioterio",
+                "status":"0"})
+    }
+})
+
+route.post('/Especie/:bioterio_id', async (req, res) => {
+    try {
+        let especie_bioterio = await prisma.especies.create({
+            data:{
+                bioterio:{connect: {id: parseInt(req.params.bioterio_id)}},
+                nome: req.body.nome,
+                quantidade: parseInt(req.body.quantidade)
+            }
+        })
+        res.json({"message":"especie cadastrada",
+                "status":"success"})
         
         }
 
     catch(e){
-
         console.log(e)
         console.log("body recebido:" + req.body)
     } finally{
-        res.json({"message":"Ocorreu um erro",
+        res.json({"message":"Ocorreu um erro em cadastro de especie",
+                "bioterio": req.params.bioterio_id, 
                 "status":"0"})
     }
-    
-})
+
+    });
+  
 module.exports = route
