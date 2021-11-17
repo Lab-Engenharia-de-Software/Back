875b297eb0a1108f27a64d0120bb3d22fb92d250
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-11-17 12:07:23 -0300

    Verificar status da votação - Joao Pedro

--- routes/vote.js
+++ routes/vote.js
@@ -3,6 +3,76 @@ var express = require('express')
 const prisma = require('../utils/prismaDB')
 var route = express.Router()
 
+//atualizar o estado atual da votação
+async function atualizarVotação(urna_id,user_id){ 
+    try{
+        let urna = await prisma.urnas.findFirst({
+            where:{
+                id: urna_id,
+                status: 'aberto'
+            }
+        })
+        console.log(urna)
+        let qntVotosSim = await prisma.votos.count({
+            where:{
+                voto: 1
+            }
+        })
+        console.log(qntVotosSim)
+        let qntVotosNão= await prisma.votos.count({
+            where:{
+                voto: 0
+            }
+        })
+        console.log(qntVotosNão)
+
+
+        if (qntVotosSim == urna.minVotos){
+            
+            //caso so possa ter 1 presidente 
+            let exPresidente = await prisma.pesquisadores.updateMany({
+                where:{
+                    role:'presidente'
+                },
+                data:{
+                    role:'pesquisador'
+                }
+            })
+
+            //eleger o novo presidente
+            let pesquisador = await prisma.pesquisadores.update({
+                where: {
+                    id: user_id,
+                },
+                data:{
+                    role: 'presidente'
+                    //status: 'pesquisador //caso seja parecerista e nao possa ser os 2 cargos
+                }
+                
+            })
+            //encerrar votação e eleger o presidente
+            return '1' //presidente eleito
+        } else if (qntVotosNão == urna.minVotos){
+            let closeUrna = await prisma.urnas.update({
+                where:{
+                    id: urna.id
+                },
+                data:{
+                    status:'fechado'
+                }
+            })
+            //encerrar votação
+            return '0' //presidente recusado
+        } else{
+            return '2' //mantem aberta
+        }
+        
+    }catch (e){
+        console.log('erro em atualizar e verificar votação', e)
+    }
+}
+
+
 //abrir votação para presidentes
 route.post('/Abrir/Presidente', async (req, res) => {
     console.log("votação aberta")
@@ -49,4 +119,5 @@ route.post('/Abrir/Presidente', async (req, res) => {
                 "status":"0"})
     }
 })
+
 module.exports = route
